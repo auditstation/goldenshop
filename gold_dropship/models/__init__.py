c06c218b87ebe505e3fc885c755d7c8fe705a20c
tech-devlp <160579593+tech-devlp@users.noreply.github.com>
2025-04-29 14:52:34 +0300

    Edit_POLINE

--- gold_dropship/models/__init__.py
+++ gold_dropship/models/__init__.py
@@ -1,6 +1,6 @@
 from . import account_tax
 from . import sale_edit
-from . import product_product_edit
+# from . import product_product_edit
 from . import product_template_edit
 from . import purchase_order_line
 from . import account_move_inherit
--- gold_dropship/models/product_template_edit.py
+++ gold_dropship/models/product_template_edit.py
@@ -10,24 +10,7 @@ _logger = logging.getLogger(__name__)
 class ProductTemplateInherit(models.Model):
     _inherit = 'product.template'
 
-    is_gold = fields.Boolean(company_dependent=True, compute='_compute_is_gold',
-                             inverse='_set_is_gold',
+    is_gold = fields.Boolean(company_dependent=True,store=True,
                              help="To refer to the products we deal with as buying and selling with gold values")
-    broken_gold = fields.Boolean(compute='_compute_broken_gold',
-                             inverse='_set_broken_gold',company_dependent=True)
+    broken_gold = fields.Boolean(store=True,company_dependent=True)
     
-    @api.depends_context('company')
-    @api.depends('product_variant_ids.is_gold')
-    def _compute_is_gold(self):
-        self._compute_template_field_from_variant_field('is_gold')
-
-    def _set_is_gold(self):
-        self._set_product_variant_field('is_gold')
-    
-    @api.depends_context('company')
-    @api.depends('product_variant_ids.broken_gold')
-    def _compute_broken_gold(self):
-        self._compute_template_field_from_variant_field('broken_gold')
-
-    def _set_broken_gold(self):
-        self._set_product_variant_field('broken_gold')
